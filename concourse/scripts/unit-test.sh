9de02c91a28874f10683ba7188b0f8816269ae1f
Jurijs Jegorovs <jurijs.jegorovs@parsiq.net>
2026-08-27 17:44:59 +0300

    fix concourse: interpolate defaults and CI script fixes

--- concourse/scripts/unit-test.sh
+++ concourse/scripts/unit-test.sh
@@ -4,7 +4,7 @@ export FOUNDRY_PROFILE="${FOUNDRY_PROFILE:-ci}"
 if ! command -v forge >/dev/null 2>&1; then
   curl -L https://foundry.paradigm.xyz | bash
   export PATH="${HOME}/.foundry/bin:${PATH}"
-  foundryup -i nightly
+  foundryup
 fi
 git submodule update --init --recursive
 forge --version
